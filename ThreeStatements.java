d54e0c212bcb1ffb3952145bceeeb92f67c5e5f2
CoderPxnk <pinkhasika@gmail.com>
2026-09-17 17:28:17 -0400

    Delete ThreeStatements.java

--- ThreeStatements.java
+++
@@ -1,9 +0,0 @@
-public class ThreeStatements
-{
-    public static void main(String[] args)
-    {
-        System.out.println("These lines will be ");
-        System.out.println("printed on ");
-        System.out.println("the new line ");
-    }
-}
